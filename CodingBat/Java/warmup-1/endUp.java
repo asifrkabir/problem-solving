0606cc6eeac6dba9c59e056feede10a9f6358bb9
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-06-08 13:31:20 +0600

    new code added

---
+++ CodingBat/Java/warmup-1/endUp.java
@@ -0,0 +1,7 @@
+public String endUp(String str) {
+  if(str.length() == 0) return str;
+  
+  if(str.length() < 3) return str.toUpperCase();
+  
+  return str.substring(0, str.length() - 3) + str.substring(str.length() - 3).toUpperCase();
+}
