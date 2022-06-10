9aa2f304bc8bd6eb6adcb71b800aec1e33600406
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-06-10 19:21:00 +0600

    new code added

---
+++ CodingBat/Java/recursion-1/count8.java
@@ -0,0 +1,13 @@
+public int count8(int n) {
+  if(n == 0) return 0;
+  
+  if(n % 10 == 8){
+    if((n / 10) % 10 == 8){
+      return 2 + count8(n / 10);
+    }
+    
+    return 1 + count8(n / 10);
+  }
+  
+  return count8(n / 10);
+}
