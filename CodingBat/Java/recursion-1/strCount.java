c73bb093b30239ae3faed9a602f5c99ec74b89e2
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-06-11 13:40:25 +0600

    new code added

---
+++ CodingBat/Java/recursion-1/strCount.java
@@ -0,0 +1,9 @@
+public int strCount(String str, String sub) {
+  if(str.length() < sub.length()) return 0;
+  
+  if(str.substring(0, sub.length()).equals(sub)){
+    return 1 + strCount(str.substring(sub.length()), sub);
+  }
+  
+  return 0 + strCount(str.substring(1), sub);
+}
