cfa44d97e38ecce12b09326cfc3d3b49ce393c7b
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-06-17 20:06:52 +0600

    new code added

--- CodingBat/Java/warmup-2/has271.java
+++ CodingBat/Java/warmup-2/has271.java
@@ -1,6 +1,6 @@
 public boolean has271(int[] nums) {
   for(int i = 0; i < nums.length - 2; i++){
-    if((nums[i + 1] - nums[i] == 5) && (nums[i] - nums[i + 2] <= 2 && nums[i] - nums[i + 2] >= 0)){
+    if((nums[i + 1] - nums[i] == 5) && (Math.abs(nums[i] - nums[i + 2] - 1) <= 2)){
       return true;
     }
   }
