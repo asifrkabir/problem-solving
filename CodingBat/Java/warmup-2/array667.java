3f02a1cb9f3d7d6c38dbf88d39e830564ae8e552
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-06-17 20:00:18 +0600

    new code added

---
+++ CodingBat/Java/warmup-2/array667.java
@@ -0,0 +1,13 @@
+public int array667(int[] nums) {
+  if(nums.length == 0) return 0;
+  
+  int count = 0;
+  
+  for(int i = 0; i < nums.length - 1; i++){
+    if(nums[i] == 6 && (nums[i + 1] == 6 || nums[i + 1] == 7)){
+      count++;
+    }
+  }
+  
+  return count;
+}
