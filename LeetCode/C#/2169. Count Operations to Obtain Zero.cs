94c4435623bdf14bbe3e785cc6133e1ba5111d46
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-07-23 17:10:46 +0600

    new code added

---
+++ LeetCode/C#/2169. Count Operations to Obtain Zero.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int CountOperations(int num1, int num2) {
+        var count = 0;
+        
+        while(num1 != 0 && num2!= 0)
+        {
+            if(num1 >= num2)
+            {
+                num1 -= num2;
+            }
+            else
+            {
+                num2 -= num1;
+            }
+            
+            count++;
+        }
+        
+        return count;
+    }
+}
