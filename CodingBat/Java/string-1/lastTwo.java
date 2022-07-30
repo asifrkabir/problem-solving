2ad9ea3b9c028d2538c88b9c9f91b8029d211331
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-07-30 19:43:40 +0600

    new code added

---
+++ CodingBat/Java/string-1/lastTwo.java
@@ -0,0 +1,5 @@
+public String lastTwo(String str) {
+  if(str.length() < 2) return str;
+  
+  return str.substring(0, str.length() - 2) + str.charAt(str.length() - 1) + str.charAt(str.length() - 2);
+}
