f193b9a47b626b8d2642aa5b62eab03e78412de7
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-08-16 20:47:02 +0600

    new problem solved

---
+++ CodingBat/Java/logic-1/squirrelPlay.java
@@ -0,0 +1,12 @@
+public boolean squirrelPlay(int temp, boolean isSummer) {
+  if(isSummer){
+    if(temp >= 60 && temp <= 100) return true;
+    
+    return false;
+  }
+  else{
+    if(temp >= 60 && temp <= 90) return true;
+    
+    return false;
+  }
+}
