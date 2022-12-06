fd5c8480276ca7ca02aa1e5b8fa7398c41752da8
asifrkabir <asifrkabir20@gmail.com>
2022-12-06 11:33:36 +0600

    problem solved

---
+++ LeetCode/C++/412. Fizz Buzz.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    vector<string> fizzBuzz(int n)
+    {
+        vector<string> answer;
+
+        for (int i = 1; i < n + 1; i++)
+        {
+            if (i % 3 == 0)
+            {
+                if (i % 5 == 0)
+                {
+                    answer.push_back("FizzBuzz");
+                }
+                else
+                {
+                    answer.push_back("Fizz");
+                }
+            }
+            else if (i % 5 == 0)
+            {
+                answer.push_back("Buzz");
+            }
+            else
+            {
+                answer.push_back(to_string(i));
+            }
+        }
+
+        return answer;
+    }
+};
