81dc0c28f4b97c7d34069ebbbfc8fd8a80ca2647
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-11-09 12:18:53 +0600

    problem solved

---
+++ LeetCode/SQL 50 - PostgreSQL/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,12 @@
+select
+    v.customer_id, count(v.visit_id) as count_no_trans
+from
+    Visits v
+left join
+    Transactions t
+on
+    v.visit_id = t.visit_id
+where
+    transaction_id is null
+group by
+    v.customer_id;
