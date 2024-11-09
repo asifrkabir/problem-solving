e80452cfa730ea3d6a18273d88da39082c826cab
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-11-09 16:05:01 +0600

    problem solved

---
+++ LeetCode/SQL 50 - PostgreSQL/1075. Project Employees I.sql
@@ -0,0 +1,14 @@
+select
+    p.project_id,
+    ROUND(
+        AVG(experience_years),
+        2
+    ) as average_years
+from
+    Project p
+join
+    Employee e
+on
+    p.employee_id = e.employee_id
+group by
+    p.project_id;
