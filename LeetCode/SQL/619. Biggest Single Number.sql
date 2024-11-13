faa678e72fe75450fb3590c68e426da6e1321e0e
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-11-13 21:39:05 +0600

    problem solved

---
+++ LeetCode/SQL/619. Biggest Single Number.sql
@@ -0,0 +1,15 @@
+select
+    coalesce(
+        (select
+            num
+        from
+            MyNumbers
+        group by
+            num
+        having
+            count(num) = 1
+        order by
+            num desc
+        limit 1),
+        null
+    ) as num;
