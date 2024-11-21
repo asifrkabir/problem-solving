31722f5463dad3aa35f1008fdb37573784d9f725
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-11-21 20:32:33 +0600

    problem solved

---
+++ LeetCode/SQL/1978. Employees Whose Manager Left the Company.sql
@@ -0,0 +1,11 @@
+select
+    employee_id
+from
+    Employees
+where
+    manager_id not in (
+        select employee_id from Employees
+    )
+    and salary < 30000
+order by
+    employee_id asc;
